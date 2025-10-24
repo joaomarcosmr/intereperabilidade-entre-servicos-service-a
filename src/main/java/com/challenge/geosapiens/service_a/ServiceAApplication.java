3811ff776e3e6bb9eea720d462e81a593746fc02
joao.rosa <joao.rosa@portobello.com.br>
2025-10-23 22:31:27 -0300

    refact: ive excluded openfeign from project because wont need

--- src/main/java/com/challenge/geosapiens/service_a/ServiceAApplication.java
+++ src/main/java/com/challenge/geosapiens/service_a/ServiceAApplication.java
@@ -2,12 +2,10 @@ package com.challenge.geosapiens.service_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableAsync
 @EnableRetry
 public class ServiceAApplication {
