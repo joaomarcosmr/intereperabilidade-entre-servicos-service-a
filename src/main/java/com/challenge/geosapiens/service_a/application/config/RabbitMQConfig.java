70f5a14d9f5a62517651e85146f2a3d9b7c722b4
joao.rosa <joao.rosa@portobello.com.br>
2025-10-23 22:28:30 -0300

    refact: autostarting queues on rabbit when starting service

--- src/main/java/com/challenge/geosapiens/service_a/application/config/RabbitMQConfig.java
+++ src/main/java/com/challenge/geosapiens/service_a/application/config/RabbitMQConfig.java
@@ -1,14 +1,17 @@
 package com.challenge.geosapiens.service_a.application.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
 
     public static final String ORDER_EXCHANGE = "order.exchange";
@@ -62,6 +65,13 @@ public class RabbitMQConfig {
         factory.setConnectionFactory(connectionFactory);
         factory.setMessageConverter(messageConverter());
         return factory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
     }
 
     @Bean
